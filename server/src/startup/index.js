d12b6c3a4d949651bee38ba7ee31a26987d6015c
ivan-khr85 <i.khreptak@apiko.com>
2019-02-14 13:52:17 +0200

    added symbol & removed unneded file

--- server/src/startup/index.js
+++ server/src/startup/index.js
@@ -6,4 +6,4 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
   .then(() => console.log('🚀 Connected to DataBase'))
-  .catch(error => console.log(`We have issue with connecting to DATABASE: ${error}`));
+  .catch(error => console.log(`💣 We have issue with connecting to DATABASE: ${error}`));
